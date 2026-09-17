9b75ac1ff191cae1107c44952c66ae53b44ef417
yoruthegoat <jcotero1@up.edu.ph>
2026-09-17 20:17:18 +0800

    Stage 4: explored SQLite

--- main.py
+++ main.py
@@ -7,6 +7,12 @@ import sqlite3
 
 app = FastAPI()
 
+# SELECT * FROM tasks; -- list every task
+# SELECT * FROM tasks WHERE done = 1; -- only completed tasks
+# SELECT COUNT(*) FROM tasks; -- how many tasks are there?
+# UPDATE tasks SET done = 1; -- mark every task completed
+# DELETE FROM tasks WHERE done = 1; -- delete all completed tasks
+
 DB_FILE = "tasks.db"
 
 def get_db():
